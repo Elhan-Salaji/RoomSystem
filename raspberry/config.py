1cdb54b7855916f7e1dd65e966213a71ce159c17
Rafail <90346190+rraaff46@users.noreply.github.com>
2026-05-15 18:05:09 +0200

    add: throughput and system metrics tracking #16

--- raspberry/config.py
+++ raspberry/config.py
@@ -5,8 +5,8 @@ ROOM_ID   = os.getenv("ROOM_ID",   "room-01")
 SENSOR_ID = os.getenv("SENSOR_ID", "sensor-01")
 
 # --- Serial ---
-SERIAL_CFG_PORT  = os.getenv("SERIAL_CFG_PORT",  "/dev/ttyUSB0")
-SERIAL_DATA_PORT = os.getenv("SERIAL_DATA_PORT", "/dev/ttyUSB1")
+SERIAL_CFG_PORT  = os.getenv("SERIAL_CFG_PORT",  "/dev/tty.usbserial-010BCEBF0")
+SERIAL_DATA_PORT = os.getenv("SERIAL_DATA_PORT", "/dev/tty.usbserial-010BCEBF1")
 SERIAL_CFG_BAUD  = 115200
 SERIAL_DATA_BAUD = 921600
 
@@ -19,6 +19,7 @@ STOMP_DESTINATION   = os.getenv("STOMP_DESTINATION",   "/app/data")
 # --- Queue & Processing ---
 QUEUE_MAX_SIZE      = int(os.getenv("QUEUE_MAX_SIZE",      "100"))
 PROCESSING_INTERVAL = float(os.getenv("PROCESSING_INTERVAL", "0.1"))  # seconds (= 10fps)
+METRICS_INTERVAL = float(os.getenv("METRICS_INTERVAL", "10")) # seconds between metric log lines
 
 # --- WebSocket Reconnect ---
 WS_RECONNECT_DELAY = int(os.getenv("WS_RECONNECT_DELAY", "5"))  # seconds until next try
